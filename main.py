c622dcbce14cf328eaaa2771cc44453e50b66f57
Keith Jung <60eokk@gmail.com>
2024-09-05 20:07:00 -0500

    added word files, and generating template

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import PyPDF2
+from docx import Document
+from docxtpl import DocxTemplate # generating template with docxtpl
 
-def extract_pdf_text(pdf_file_path):
+
+
+# FOR PDF FILES
+def extract_pdf(pdf_file_path):
     with open(pdf_file_path, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
         text = ""
@@ -8,5 +13,34 @@ def extract_pdf_text(pdf_file_path):
             text += reader.pages[page_num].extract_text()
         return text
 
-pdf_text = extract_pdf_text('pdf_word.pdf')
+pdf_text = extract_pdf('your_pdf.pdf')
 print(pdf_text)
+
+
+# FOR WORD FILES
+def extract_word(docx_file_path):
+    doc = Document(docx_file_path)
+    text = ""
+    for paragraph in doc.paragraphs:
+        text += paragraph.text + '\n'
+    return text
+
+word_text = extract_word('your_docx.docx')
+print(word_text)
+
+
+
+
+def generate_template(template_path, output_path, context):
+    doc = DocxTemplate(template_path)
+    doc.render(context)
+    doc.save(output_path)
+
+# the context for the template
+context = {
+    'title': 'Document Title',
+    'section_heading': 'Introduction',
+    'content': 'This is the populated content for the section.'
+}
+
+generate_template('template.docx', 'output.docx', context)
